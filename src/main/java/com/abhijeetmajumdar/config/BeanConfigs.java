f8dc4ca3b78ecddd9298b8e7273973c2f2bb28bc
Abhijeet Majumdar <abhijeet_majumdar@yahoo.com>
2025-06-08 17:05:37 -0400

    Draft version

--- src/main/java/com/abhijeetmajumdar/config/BeanConfigs.java
+++ src/main/java/com/abhijeetmajumdar/config/BeanConfigs.java
@@ -1,27 +1,49 @@
 package com.abhijeetmajumdar.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaModel;
+import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfigs {
-    private final ChatClient.Builder builder;
-    @Autowired
-    private ToolCallbackProvider toolCallbackProvider;
 
-    public BeanConfigs(ChatClient.Builder builder) {
-        this.builder = builder;
+    @Bean
+    public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
+
+
+    @Bean
+    public MessageChatMemoryAdvisor messageChatMemoryAdvisor(ChatMemory chatMemory) {
+        return new MessageChatMemoryAdvisor(chatMemory);
     }
 
     @Bean
-    public ChatClient chatClient() {
-
-        return this.builder
-                .defaultTools(toolCallbackProvider.getToolCallbacks())
+    public ChatClient myChatClient(ChatClient.Builder builder, ToolCallbackProvider toolCallbackProvider, MessageChatMemoryAdvisor messageChatMemoryAdvisor) {
+        ChatModel chatModel = OllamaChatModel.builder()
+                .ollamaApi(OllamaApi.builder().build())
+                .defaultOptions(OllamaOptions.builder()
+                        .model(OllamaModel.LLAMA3_2_1B)
+                        .temperature(0.6)
+                        .toolCallbacks(toolCallbackProvider.getToolCallbacks())
+                        .build())
                 .build();
+
+        ChatClient chatClient = ChatClient.builder(chatModel)
+                .defaultSystem("You are Chase Bank assistant, and a friendly Chat bot. ")
+                .defaultAdvisors(messageChatMemoryAdvisor)
+                .build();
+
+        return chatClient;
     }
 
 }
--- src/main/java/com/abhijeetmajumdar/controllers/ChatController.java
+++ src/main/java/com/abhijeetmajumdar/controllers/ChatController.java
@@ -3,28 +3,45 @@ package com.abhijeetmajumdar.controllers;
 
 import com.abhijeetmajumdar.models.ChatRequest;
 import com.abhijeetmajumdar.models.ChatResponse;
-import jakarta.annotation.Resource;
+import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.spec.McpSchema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController {
+    private final static Logger LOG = LoggerFactory.getLogger(ChatController.class);
 
+    @Autowired
+    private ChatClient myChatClient;
 
-    @Resource
-    private ChatClient chatClient;
+    @Autowired
+    List<McpSyncClient> mcpSyncClients;
+
+    @Autowired
+    ToolCallbackProvider toolCallbackProvider;
 
     @PostMapping
     public ResponseEntity<ChatResponse> chat(@RequestBody ChatRequest request) {
         try {
-
+            McpSyncClient syncClient = mcpSyncClients.get(0);
+            System.out.println(syncClient.listTools());
+            System.out.println(syncClient.callTool(new McpSchema.CallToolRequest("findAllChaseOfferings", Map.of("", ""))));
             String userMessage = request.getMessage();
-            String content = chatClient.prompt()
+
+            String content = myChatClient.prompt()
                     .user(userMessage)
                     .call()
                     .content();
@@ -35,5 +52,6 @@ public class ChatController {
             return ResponseEntity.ok(new ChatResponse("Ollama: " + e.getMessage()));
         }
     }
+
 
 }
